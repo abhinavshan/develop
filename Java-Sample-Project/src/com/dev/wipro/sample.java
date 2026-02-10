13de752a683c78934eb0b3181e832ea8e3568ac2
Abhinav Shankar <abhinav.shankar@wipro.com>
2026-02-10 17:24:25 +0530

    adding new code

--- Java-Sample-Project/src/com/dev/wipro/sample.java
+++ Java-Sample-Project/src/com/dev/wipro/sample.java
@@ -1,8 +1,22 @@
 package src.com.dev.wipro;
 
+
+
 public class sample {
   public static	void main(String [] arg) {
 	  System.out.println("Hello world");
+        sample ob = new sample();
+        ob.m1(null);
   }
+
+  void m1(Object o)
+  {
+    System.out.println(o);
+  }
+
+    void m1(String o)
+    {
+        System.out.println(o);
+    }
 
 }
---
+++ QuantityMeasurementApp/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,20 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+
+  private static class Feet {
+
+      public Feet(Double value){
+
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+          return super.equals(obj);
+      }
+  }
+    public static void main(String[] args) {
+       QuantityMeasurementApp ref = new QuantityMeasurementApp();
+
+    }
+}
