6b6d4262e7878aef5efc79e9cb489fa7a9595ef3
Abhinav Shankar <abhinav.shankar@wipro.com>
2026-02-13 11:39:52 +0530

    Writing the code for UseCase-1

---
+++ QuantityMeasurementApp/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,32 @@
+package com.apps.quantitymeasurement;
+
+/**
+ * @author  : Abhinav.Shankar@wipro.com
+ * Descriptoin : create the object and do comparison for double value
+ */
+public class QuantityMeasurementApp {
+
+    private static class Feet {
+
+        public Feet(Double value){
+
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+    }
+    public static void main(String[] args) {
+        Feet feetObj1 = new Feet(7.99);
+        Feet feetObj2 = new Feet(7.99);
+
+        if (feetObj1.equals(feetObj2)){
+           System.out.println("printing value for Feet Obj ---> " + Boolean.TRUE);
+        }
+        else {
+            System.out.println("printing value for Feet Obj ---> " + Boolean.FALSE);
+
+        }
+    }
+}
